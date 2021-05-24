4601a5e45df5e88e9e57b354c5b69ee04240ada2
Alexander Bagatka <alex.bagatka@gmail.com>
2021-05-25 00:14:43 +0300

    Add environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCslhxVvWKTf8PTUpwrseIzdndjukMDh18',
+    authDomain: 'asenkaguideprod.firebaseapp.com',
+    databaseURL: 'https://asenkaguideprod-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'asenkaguideprod',
+    storageBucket: 'asenkaguideprod.appspot.com',
+    messagingSenderId: '956077585384',
+    appId: '1:956077585384:web:cc213ab7fa5f85d8532a5d',
+    measurementId: 'G-8PF9TVCKCE'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBz23skVTfkJg_kV9xa9wB_4z6XrRZLakw',
+    authDomain: 'asenkaguide.firebaseapp.com',
+    databaseURL: 'https://asenkaguide-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'asenkaguide',
+    storageBucket: 'asenkaguide.appspot.com',
+    messagingSenderId: '149408008764',
+    appId: '1:149408008764:web:363c4b134abc8b7fbb5139',
+    measurementId: 'G-NLHB92K4NN'
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
